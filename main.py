6a97f678b46cc96f740ec3f23f43f35d0892f211
sergei <Tsubak193@mail.ru>
2021-06-28 13:22:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+
 print("Hello from Sergei Emelyanov")
+
+requests.get('https://playground.learnqa.ru/api/get_text')
+print(requests.get('https://playground.learnqa.ru/api/get_text'))
+
+
+
