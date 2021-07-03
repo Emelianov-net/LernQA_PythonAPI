4cbf13271b5497507d04b3e5de9c95f985ae516d
sergei <Tsubak193@mail.ru>
2021-07-03 09:20:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 print("Hello from Sergei Emelyanov")
 
 result = requests.get('https://playground.learnqa.ru/api/get_text')
-
 print(result.text)
 
+payload = {"name": "user"}
+result = requests.get('https://playground.learnqa.ru/api/hello', params=payload)
+print(result.text)
 
 
